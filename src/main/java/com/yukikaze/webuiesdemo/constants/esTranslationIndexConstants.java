af135a6ce4ec3fc147d154e14d998adc50a21d71
1yukikaze <121788310+1yukikaze@users.noreply.github.com>
2023-02-21 02:13:46 +0800

    索引调整

--- src/main/java/com/yukikaze/webuiesdemo/constants/esTranslationIndexConstants.java
+++ src/main/java/com/yukikaze/webuiesdemo/constants/esTranslationIndexConstants.java
@@ -1,50 +1,100 @@
 package com.yukikaze.webuiesdemo.constants;
 
 //索引库创建json,创建方法在test目录下
+@SuppressWarnings("unused")
 public class esTranslationIndexConstants {
-    public static final String MAPPING_TEXT1 = "{\n" +
-            "  \"settings\": {\n" +
-            "    \"analysis\": {\n" +
-            "      \"analyzer\": {\n" +
-            "        \"text_analysis\":{\n" +
-            "          \"tokenizer\": \"ik_smart\",\n" +
-            "          \"filter\": \"py\"\n" +
-            "        },\n" +
-            "        \"completion_analyzer\": {\n" +
-            "          \"tokenizer\": \"keyword\",\n" +
-            "          \"filter\": \"py\"\n" +
-            "        }\n" +
-            "      },\n" +
-            "      \"filter\":{\n" +
-            "        \"py\":{\n" +
-            "          \"type\": \"pinyin\",\n" +
-            "          \"keep_full_pinyin\": false,\n" +
-            "          \"keep_joined_full_pinyin\": true,\n" +
-            "          \"keep_original\": true,\n" +
-            "          \"limit_first_letter_length\": 16,\n" +
-            "          \"remove_duplicated_term\": true,\n" +
-            "          \"none_chinese_pinyin_tokenize\": false\n" +
-            "        }\n" +
-            "      }\n" +
-            "    }\n" +
-            "  },\n" +
-            "  \"mappings\": {\n" +
-            "    \"properties\": {\n" +
-            "      \"chineseTag\":{\n" +
-            "        \"type\": \"text\",\n" +
-            "        \"index\": true, \n" +
-            "        \"analyzer\": \"text_analysis\",\n" +
-            "        \"search_analyzer\": \"ik_smart\"\n" +
-            "      },\n" +
-            "      \"englishTag\":{\n" +
-            "        \"type\": \"keyword\",\n" +
-            "        \"index\": false\n" +
-            "      },\n" +
-            "      \"suggestion\":{\n" +
-            "        \"type\": \"completion\",\n" +
-            "        \"analyzer\": \"completion_analyzer\"\n" +
-            "      }\n" +
-            "    }\n" +
-            "  }\n" +
-            "}";
+    public static final String MAPPING_TEXT1 = """
+            {
+              "settings": {
+                "analysis": {
+                  "analyzer": {
+                    "text_analysis":{
+                      "tokenizer": "ik_smart",
+                      "filter": "py"
+                    },
+                    "completion_analyzer": {
+                      "tokenizer": "keyword",
+                      "filter": "py"
+                    }
+                  },
+                  "filter":{
+                    "py":{
+                      "type": "pinyin",
+                      "keep_full_pinyin": false,
+                      "keep_joined_full_pinyin": true,
+                      "keep_original": true,
+                      "limit_first_letter_length": 16,
+                      "remove_duplicated_term": true,
+                      "none_chinese_pinyin_tokenize": false
+                    }
+                  }
+                }
+              },
+              "mappings": {
+                "properties": {
+                  "chineseTag":{
+                    "type": "text",
+                    "index": true,\s
+                    "analyzer": "text_analysis",
+                    "search_analyzer": "ik_smart"
+                  },
+                  "englishTag":{
+                    "type": "keyword",
+                    "index": false
+                  },
+                  "suggestion":{
+                    "type": "completion",
+                    "analyzer": "completion_analyzer"
+                  }
+                }
+              }
+            }""";
+
+    public static final String MAPPING_TEXT2= """
+            PUT /es_translation_index
+            {
+              "settings": {
+                "analysis": {
+                  "analyzer": {
+                    "text_analysis":{
+                      "tokenizer": "ik_smart",
+                      "filter": "py"
+                    },
+                    "completion_analyzer": {
+                      "tokenizer": "keyword",
+                      "filter": "py"
+                    }
+                  },
+                  "filter":{
+                    "py":{
+                      "type": "pinyin",
+                      "keep_full_pinyin": false,
+                      "keep_joined_full_pinyin": true,
+                      "keep_original": true,
+                      "limit_first_letter_length": 16,
+                      "remove_duplicated_term": true,
+                      "none_chinese_pinyin_tokenize": false
+                    }
+                  }
+                }
+              },
+              "mappings": {
+                "properties": {
+                  "chineseTag":{
+                    "type": "text",
+                    "index": true,
+                    "analyzer": "text_analysis",
+                    "search_analyzer": "ik_max_word"
+                  },
+                  "englishTag":{
+                    "type": "keyword",
+                    "index": false
+                  },
+                  "suggestion":{
+                    "type": "completion",
+                    "analyzer": "completion_analyzer"
+                  }
+                }
+              }
+            }""";
 }
