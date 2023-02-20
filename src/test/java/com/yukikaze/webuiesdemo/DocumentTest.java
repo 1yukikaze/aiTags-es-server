563db748fba8932c18faac931c88db934ab83421
1yukikaze <121788310+1yukikaze@users.noreply.github.com>
2023-02-20 17:40:53 +0800

    索引库设计导入测试

--- src/test/java/com/yukikaze/webuiesdemo/DocumentTest.java
+++ src/test/java/com/yukikaze/webuiesdemo/DocumentTest.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.yukikaze.webuiesdemo.pojo.TranslationDoc;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.bulk.BulkRequest;
-import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
@@ -13,10 +12,12 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 import java.util.Set;
@@ -45,7 +46,9 @@ class DocumentTest {
         InputStream inputStream = new FileInputStream("src/test/resources/prop.properties");
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
         prop.load(inputStreamReader);
-        System.out.println(prop.toString());
+        //测试
+        System.out.println(prop);
+
         //遍历存入对象
         Set<Object> keySet = prop.keySet();
         BulkRequest request = new BulkRequest(); //导入方法
@@ -53,7 +56,9 @@ class DocumentTest {
             TranslationDoc translationDoc = new TranslationDoc();
             translationDoc.setChineseTag((String) key);
             translationDoc.setEnglishTag((String) prop.get(key));
-            System.out.println(translationDoc.toString());
+            //测试
+            System.out.println(translationDoc);
+
             request.add(new IndexRequest("es_translation_index")
                             .id(UUID.randomUUID().toString())
                             .source(JSON.toJSONString(translationDoc), XContentType.JSON));
